c07be1fafd2c5ea6792b459e986ff7bfc69d0e64
thanhtrung237 <nthanhtrung2307@gmail.com>
2026-02-08 09:00:00 +0700

    feat(user-service): add UserRepository with findByEmail and existsByEmail

---
+++ techshop-microservice/user-service/src/main/java/com/techshop/userservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.techshop.userservice.repository;
+
+import com.techshop.userservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
