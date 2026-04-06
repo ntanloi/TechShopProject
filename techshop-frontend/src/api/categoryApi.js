6e7835d673cffd8dacc6d3f79381a418d59bb49f
TranCongMinh14 <tcminhangiang@gmail.com>
2026-04-06 13:31:13 +0700

    feat(fe-api): add categoryApi for product categorization queries

---
+++ techshop-frontend/src/api/categoryApi.js
@@ -0,0 +1,12 @@
+import axiosClient from "./axios";
+import { API_ROUTES } from "./routes";
+
+const categoryApi = {
+  getAll: () => axiosClient.get(API_ROUTES.categories.list),
+  getById: (id) => axiosClient.get(API_ROUTES.categories.detail(id)),
+  create: (data) => axiosClient.post(API_ROUTES.categories.create, data),
+  update: (id, data) => axiosClient.put(API_ROUTES.categories.update(id), data),
+  delete: (id) => axiosClient.delete(API_ROUTES.categories.delete(id)),
+};
+
+export default categoryApi;
