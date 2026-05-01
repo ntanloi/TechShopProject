9686106544bb413b36d8187b2e80c7f83933d8a7
Nguyen Ho Thanh Minh <nghothminz@gmail.com>
2026-05-01 10:14:25 +0700

    Update manage-product,category. Fix image, validate

--- techshop-microservice/product-service/src/main/java/com/techshop/productservice/repository/CategoryRepository.java
+++ techshop-microservice/product-service/src/main/java/com/techshop/productservice/repository/CategoryRepository.java
@@ -2,6 +2,15 @@ package com.techshop.productservice.repository;
 
 import com.techshop.productservice.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    
+    boolean existsByName(String name);
+    
+    Category findByName(String name);
+    
+    @Query("SELECT COUNT(p) FROM Product p WHERE p.category.id = :categoryId")
+    long countProductsByCategoryId(@Param("categoryId") Long categoryId);
 }
